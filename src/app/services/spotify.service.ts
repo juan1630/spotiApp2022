7d7c3a90d75a1e016df41b166920e23df7697ea2
Juan Patrón <josejuanpatron1630@gmail.com>
2022-02-20 13:00:36 -0600

    Refactor de las peticiones GET

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,27 +10,30 @@ export class SpotifyService {
     private _http:HttpClient
   ) { }
 
-  // se crea una sola instacia del servicio en el toda la app
+  //refactor de las peticiones
+  getQuery(  query: string ){
+       query.trim();
 
-  getNewRealeses() {
-    // se modifican los headers de la peticion
+       // se modifican los headers de la peticion
     const headers =  new HttpHeaders({
-      'Authorization' :"Bearer BQDIUQddSYfuaOY_JmUMy9kOBfbStEjaac8aX0V05l6YVTftY6GxRjoQiWqW_xGTu6TTbHG91Xx3XtipyO8"  
+      'Authorization' :"Bearer BQDnezJRrbz_7lkLsitfFkL0upRjn13RuNgb3MZAy-lGV3DeHFdhA5QRFZVoOgmY71mOlxinU8xa3boXD3g"  
     });
 
     // se agregan los headers para la aplicacion
-    return this._http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+    return this._http.get('https://api.spotify.com/v1/'+ query, {headers});
+
+  }
+
+  // se crea una sola instacia del servicio en el toda la app
+  getNewRealeses() {
+    return this.getQuery('browse/new-releases');
   }
 
   getArtista( termino: string ) {
 
     termino.trim();
 
-    const headers =  new HttpHeaders({
-      'Authorization' :"Bearer BQDIUQddSYfuaOY_JmUMy9kOBfbStEjaac8aX0V05l6YVTftY6GxRjoQiWqW_xGTu6TTbHG91Xx3XtipyO8"  
-    });
-
-    return this._http.get('https://api.spotify.com/v1/search?q='+ termino +'&type=artist', { headers});
+    return this.getQuery('search?q='+ termino +'&type=artist');
   }
 
 }
